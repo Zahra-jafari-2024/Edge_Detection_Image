bf7ccf8a8858912d8ddda2930b90466253694da9
Zahra-jafari-2024 <161893629+Zahra-jafari-2024@users.noreply.github.com>
2024-06-01 00:38:56 +0330

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,21 +1,23 @@
-from setuptools import setup
-
-with open("./README.md", "r") as f:
-    long_description = f.read()
-
-with open('./requirements.txt') as f:
-    install_requires = f.read().splitlines()
-
+from setuptools import setup, find_packages
 
 setup(
-    name="Edge_Detection_Image",
-    version="0.1.0", 
-    author="Zahra Jafari", 
-    description="image processing technique for finding the boundaries of objects within images",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
+    name='Edge_Detection_Image',
+    version='0.1.0',
+    packages=find_packages(),
+    description="Edge Detection is a method of segmenting an image into regions of discontinuity",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author="zahra jafari",
+    author_email="mzahrajafari94@.com",
     url="https://github.com/Zahra-jafari-2024/Edge_Detection_Image",
-    install_requires=install_requires,
-    author_email="mzahrajafari94@gmail.com",
-    packages=["Edge_Detection_Image"]
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'click',
+    ],
+    
+    python_requires='>=3.6',
 )

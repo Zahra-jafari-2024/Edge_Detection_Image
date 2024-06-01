db64cc49c85ddcac8ae7ae8bdf75ee0372da6e2d
Zahra-jafari-2024 <161893629+Zahra-jafari-2024@users.noreply.github.com>
2024-06-01 09:05:23 +0330

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='Edge_Detection_Image',
-    version='0.1.0',
+    name='Edge_Detection_Images',
+    version='0.1.1',
     packages=find_packages(),
     description="Edge Detection is a method of segmenting an image into regions of discontinuity",
     long_description=open('README.md').read(),
